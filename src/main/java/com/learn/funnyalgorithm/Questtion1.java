b0b01bb57305608220ef72cd73618647fe3e6690
nardoge <nardoge@163.com>
2018-03-13 22:31:55 +0800

    fix():Q1修改

--- src/main/java/com/learn/funnyalgorithm/Questtion1.java
+++ src/main/java/com/learn/funnyalgorithm/Questtion1.java
@@ -1,9 +1,15 @@
 package com.learn.funnyalgorithm;
 
+/**
+ *如果把某个数的各个数字按相反的顺序排列，得到的数和原来的数相同，则这个数就是“回文数”。譬如 123454321 就是一个回文数。
+
+ 求用十进制、二进制、八进制表示都是回文数的所有数字中，大于十进制数 10 的最小值。 增加限制:二进制不补0
+ */
 public class Questtion1 {
 
     public static void main(String[] args) {
 
+        //--------------------------------------很大气版本begin----------------------------------------------------
         int num = 11;
         while (true) {
             String numTenStr = String.valueOf(num);
@@ -24,5 +30,23 @@ public class Questtion1 {
             num += 2;
         }
 
+        //--------------------------------------很大气版本end----------------------------------------------------
+        System.out.println(isPalindromic(505));
     }
+
+    //--------------------------------------小气提示begin----------------------------------------------------
+    public static boolean isPalindromic(final int num){
+        if (num < 11){
+            return false;
+        }
+        int reverse = 0;
+        int origin = num;
+        while (origin > 0){
+            reverse = reverse * 10 + origin % 10;
+            origin /= 10;
+        }
+
+        return reverse == num;
+    }
+    //--------------------------------------小气提示end----------------------------------------------------
 }
