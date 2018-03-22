6c8cd5c6979b578aff4942f06dd02d1df0fab0a6
nardoge <nardoge@163.com>
2018-03-22 23:50:05 +0800

    fix():rename

--- src/main/java/com/learn/funnyalgorithm/Question1.java
+++ src/main/java/com/learn/funnyalgorithm/Question1.java
@@ -5,12 +5,12 @@ package com.learn.funnyalgorithm;
 
  求用十进制、二进制、八进制表示都是回文数的所有数字中，大于十进制数 10 的最小值。 增加限制:二进制不补0
  */
-public class Questtion1 {
+public class Question1 {
 
     public static void main(String[] args) {
-
-        //--------------------------------------很大气版本begin----------------------------------------------------
+        // 大于10，所以从11开始
         int num = 11;
+
         while (true) {
             String numTenStr = String.valueOf(num);
             StringBuilder numTenStrSB = new StringBuilder(numTenStr);
@@ -30,23 +30,5 @@ public class Questtion1 {
             num += 2;
         }
 
-        //--------------------------------------很大气版本end----------------------------------------------------
-        System.out.println(isPalindromic(505));
     }
-
-    //--------------------------------------小气提示begin----------------------------------------------------
-    public static boolean isPalindromic(final int num){
-        if (num < 11){
-            return false;
-        }
-        int reverse = 0;
-        int origin = num;
-        while (origin > 0){
-            reverse = reverse * 10 + origin % 10;
-            origin /= 10;
-        }
-
-        return reverse == num;
-    }
-    //--------------------------------------小气提示end----------------------------------------------------
 }
